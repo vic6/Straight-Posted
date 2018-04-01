26e9730fcc254823a6cd1e3b0649db05484db977
vic6 <vicmatthews116@gmail.com>
2018-03-31 22:00:16 -0700

    Add and configure cors, add create action

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,8 +1,19 @@
 module Api::V1
   class IdeasController < ApplicationController
     def index
-      @ideas = Idea.all
+      @ideas = Idea.order('created_at DESC')
       render json: @ideas
+    end
+
+    def create
+      @idea = Idea.create(idea_params)
+      render json: @idea
+    end
+
+    private
+
+    def idea_params
+      params.require(:idea).permit(:title, :body)
     end
   end
 end
